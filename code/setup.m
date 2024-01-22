aca7c86bd788b5eeac77e4c6700ddf5bd9a307f0
silviufilote <fsilviu89@gmail.com>
2024-01-22 13:40:20 +0100

    update convergence

--- code/setup.m
+++ code/setup.m
@@ -1,5 +1,6 @@
 clc
 clearvars
+close all
 
 addpath('../D-STEM/Src/');
 
@@ -84,7 +85,6 @@ end
 traffic.mean_prec{1} = mean_prec;
 traffic.mean_temp{1} = mean_temp;
 
-
 % Traffic routes variables
 route_type = zeros(ns,3);
 route_type(:,1) = utah_meta(:,5);
@@ -92,26 +92,151 @@ route_type(:,2) = utah_meta(:,6);
 route_type(:,3) = utah_meta(:,7);
 traffic.route_type{1} = route_type;
 
-clear c m mean_prec mean_temp route_type
-clear utah_traffic utah_temp utah_prec utah_meta
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%          Renaming       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-traffic.Y_name{1} = {'traffic'};
+traffic.Y_name{1} = 'traffic';
 
 % Creating X_beta
-X_beta = zeros(ns, 4, T);
+X_beta = zeros(ns, 7, T);
 X_beta(:,1,:) = repelem(traffic.weekend{1}', ns, 1);
 X_beta(:,2,:) = repelem(traffic.holiday{1}', ns, 1);
 X_beta(:,3,:) = repelem(traffic.mean_temp{1}', ns, 1);
 X_beta(:,4,:) = repelem(traffic.mean_prec{1}', ns, 1);
+X_beta(:,5,:) = repelem(utah_meta(:,5), 1, T);
+X_beta(:,6,:) = repelem(utah_meta(:,6), 1, T);
+X_beta(:,7,:) = repelem(utah_meta(:,7), 1, T);
+
 traffic.X_beta{1} = X_beta;  
-traffic.X_beta_name{1} = {'weekend', 'holdays', 'mean temp', 'mean prec'};
+traffic.X_beta_name{1} = {'weekend', 'holidays', 'mean temp', 'mean prec', 'interstate', 'US', 'RS'};
 
-traffic.X_p = traffic.route_type;
-traffic.X_p_name{1}  = {'interstate','US','RS'};
+traffic.X_spa = traffic.route_type;
+traffic.X_spa_name{1}  = {'interstate', 'US', 'RS'};
 
+clear c m mean_prec mean_temp route_type
+clear utah_traffic utah_temp utah_prec utah_meta
 clear X_beta ns T
+
 save('traffic.mat');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%      Operation on the Y     %%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Variables
+ns = size(traffic.Y{1}, 1);                         % number of stations    
+T = size(traffic.Y{1}, 2);                          % number of time steps
+st = 3;                                             % station considered
+
+% Stationary process?
+% Differencing order 1 -> delete the trend 
+
+% adftest       H0: the time series contains a unit root and is non-stationary 
+% kpsstest      H0: that the time series is stationary
+% vratiotest    H0: no random walks constant variance 
+% lbqtest       H0: no residual autocorrelation
+
+% [h1,pValue1] = adftest(diff(traffic.Y{1}(3,:)));                % unit root? 1 - trend stationary
+% [h2,pValue2] = kpsstest(traffic.Y{1}(3,:));                     % unit root? 0 - trend stationary
+% [h3,pValue3] = vratiotest(traffic.Y{1}(3,:));                   % random walks? heteroschedasticity
+
+
+tiledlayout(3,4)
+
+nexttile
+hold on
+plot(traffic.Y{1}(st,:));
+title(['Time series ',num2str(st),'rd station']);
+
+nexttile
+hold on
+autocorr(traffic.Y{1}(st,:),40);
+title(['Autocorrelation ',num2str(st),'rd station']);
+
+nexttile
+hold on
+plot(diff(traffic.Y{1}(st,:),2));
+title(['Diff ',num2str(st),'rd station']);
+
+nexttile
+hold on
+autocorr(diff(traffic.Y{1}(st,:),2), 40);
+title(['Diff ',num2str(st),'rd autocorrelation']);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                     Data cleaning and seasoned                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% removing first 10 data -> more consistent
+% Differencing: stabilize mean, seasonality and trend
+
+s_data = 10;                                        % data initial drop 
+ns = size(traffic.Y{1}, 1);                         % number of stations    
+T = size(traffic.Y{1}, 2) - s_data + 1;             % number of time steps - data drop + 1 partenza al 10*
+freq_seasoned = 24;                                 % frequency seasonality
+D = LagOp({1 -1},'Lags',[0, freq_seasoned]);        % lag operator seasonality
+
+
+traffic_season = ones(ns, T - freq_seasoned);
+for x = 1:ns
+    traffic_season(x,:) = filter(D, traffic.Y{1}(x,s_data:end));
+end
+
+
+
+dtraffic.Y{1} = traffic_season(:,:);
+dtraffic.Y_name{1} = 'traffic';
+dtraffic.weekend{1} = traffic.weekend{1}(s_data:end - freq_seasoned,1);                                                       
+dtraffic.holiday{1} = traffic.holiday{1}(s_data:end - freq_seasoned,1);
+dtraffic.mean_prec{1} = traffic.mean_prec{1}(s_data:end - freq_seasoned,1); 
+dtraffic.mean_temp{1} = traffic.mean_temp{1}(s_data:end - freq_seasoned,1);
+dtraffic.latitude = traffic.latitude;
+dtraffic.longitude = traffic.longitude;
+dtraffic.route = traffic.route;
+dtraffic.station_id = traffic.station_id;
+dtraffic.time_ini = '01-01-2022 9:00';
+dtraffic.time_fin = '30-01-2022 23:00';
+
+
+% Creating X_beta
+T = size(dtraffic.Y{1}, 2);
+
+X_beta = zeros(ns, 7, T);
+X_beta(:,1,:) = repelem(dtraffic.weekend{1}', ns, 1);
+X_beta(:,2,:) = repelem(dtraffic.holiday{1}', ns, 1);
+X_beta(:,3,:) = repelem(dtraffic.mean_temp{1}', ns, 1);
+X_beta(:,4,:) = repelem(dtraffic.mean_prec{1}', ns, 1);
+X_beta(:,5,:) = repelem(traffic.route_type{1}(:,1), 1, T);
+X_beta(:,6,:) = repelem(traffic.route_type{1}(:,2), 1, T);
+X_beta(:,7,:) = repelem(traffic.route_type{1}(:,3), 1, T);
+
+dtraffic.X_beta{1} = X_beta;  
+dtraffic.X_beta_name{1} = {'weekend', 'holidays', 'mean temp', 'mean prec', 'interstate', 'US', 'RS'};
+
+dtraffic.X_spa = traffic.route_type;
+dtraffic.X_spa_name{1}  = {'interstate', 'US', 'RS'};
+
+nexttile
+hold on
+plot(dtraffic.Y{1}(st,:));
+title(['Seasoned/cleaned ',num2str(st),'rd station time series'])
+
+nexttile
+hold on
+autocorr(dtraffic.Y{1}(st,:), 40);
+title(['Seasoned/cleaned ',num2str(st),'rd station autocorrelation'])
+
+nexttile
+hold on
+plot(diff(dtraffic.Y{1}(st,:)));
+title(['Seasoned/cleaned ',num2str(st),'rd station + diff time series'])
+
+nexttile
+hold on
+autocorr(diff(dtraffic.Y{1}(st,:)), 40);
+title(['Seasoned/cleaned ',num2str(st),'rd station + diff autocorrelation'])
+
+clear traffic s_data ns T st freq_seasoned D12 traffic_season x D X_beta
+save('dtraffic.mat');
+
